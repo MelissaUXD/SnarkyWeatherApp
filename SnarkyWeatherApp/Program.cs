8fb5bbeeae5c8a8e057fbea938b61f7a6453d8db
Melissa Updike <Melissa Updike@DESKTOP-KTRJU7I>
2022-09-30 09:16:59 -0400

    Add project files.

---
+++ SnarkyWeatherApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+public class SnarkyWeatherApp;
+var temperature();
+int humidity = X; 
+public string conditions = "rainy"  { get; set; }
+
+var[] highTemp = new highTemp[];
+double[] lowTemp = new double[0];
+double[] justRightTemp= new double[0];
+
+int[] highHumidity = new int[0];
+int[] notHumid = new int[0];
+
+string[] weatherConditions = new string[] {"Raining", "Snowing", "Foggy", "Sunny", "Freezing Rain",};
